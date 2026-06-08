b1c1261a42a1e18f2a669ab13cd27e91b265fd28
Ayushmaan Mishra <ayush99wiz@gmail.com>
2026-06-08 22:12:06 +0530

    updates

--- src/constants/site.js
+++ src/constants/site.js
@@ -1,3 +1,4 @@
 export const BRAND_NAME = "SkillSphere";
 export const MAIN_SITE_URL = "https://skillsphere.space/";
 export const SUPPORT_EMAIL = "support@skillsphere.space";
+// these re necessary for the app to work, so we export them as constants
